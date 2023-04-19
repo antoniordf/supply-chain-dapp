a395fdccb29507d06e9cda59543ea0324c9f8324
Antonio Furtado <antoniordf@hotmail.com>
2023-04-19 14:10:17 +0100

    implemented the processItem test

--- project-6/test/TestSupplychain.js
+++ project-6/test/TestSupplychain.js
@@ -111,14 +111,23 @@ contract("SupplyChain", function (accounts) {
     const supplyChain = await SupplyChain.deployed();
 
     // Declare and Initialize a variable for event
+    let eventEmitted = false;
 
     // Watch the emitted event Processed()
+    const event = supplyChain.Processed();
+    await event.watch((err, res) => {
+      eventEmitted = true;
+    });
 
     // Mark an item as Processed by calling function processtItem()
+    await supplyChain.processItem(upc);
 
     // Retrieve the just now saved item from blockchain by calling function fetchItem()
+    const resultBufferTwo = await supplyChain.fetchItemBufferTwo.call(upc);
 
     // Verify the result set
+    assert.equal(resultBufferTwo[5], 1, "Error: Invalid item State");
+    assert.equal(eventEmitted, true, "Invalid event emitted");
   });
 
   // 3rd Test
