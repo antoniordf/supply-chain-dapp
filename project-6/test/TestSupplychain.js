c1954eddb6514ee0f8e79d8b56310691ab3bbdf0
Antonio Furtado <antoniordf@hotmail.com>
2023-04-19 14:27:26 +0100

    Implemented sellItem() test

--- project-6/test/TestSupplychain.js
+++ project-6/test/TestSupplychain.js
@@ -138,7 +138,7 @@ contract("SupplyChain", function (accounts) {
     let eventEmitted = false;
 
     // Watch the emitted event Packed()
-    const event = supplyChain.Processed();
+    const event = supplyChain.Packed();
     await event.watch((err, res) => {
       eventEmitted = true;
     });
@@ -159,14 +159,23 @@ contract("SupplyChain", function (accounts) {
     const supplyChain = await SupplyChain.deployed();
 
     // Declare and Initialize a variable for event
+    let eventEmitted = false;
 
-    // Watch the emitted event ForSale()
+    // Watch the emitted event Sold()
+    const event = supplyChain.Sold();
+    await event.watch((err, res) => {
+      eventEmitted = true;
+    });
 
     // Mark an item as ForSale by calling function sellItem()
+    await supplyChain.sellItem(upc);
 
     // Retrieve the just now saved item from blockchain by calling function fetchItem()
+    const resultBufferTwo = await supplyChain.fetchItemBufferTwo.call(upc);
 
     // Verify the result set
+    assert.equal(resultBufferTwo[5], 3, "Error: Invalid item State");
+    assert.equal(eventEmitted, true, "Invalid event emitted");
   });
 
   // 5th Test
