f467dfb6584d16a6ea7902a0a332c9927adc94ec
Antonio Furtado <antoniordf@hotmail.com>
2023-04-20 22:34:11 +0100

    Did all the configuration to deploy to sepolia

--- project-6/truffle-config.js
+++ project-6/truffle-config.js
@@ -10,6 +10,18 @@ module.exports = {
       port: 8545,
       network_id: "*", // Match any network id
     },
+    sepolia: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://sepolia.infura.io/v3/${infuraID}`
+        ),
+      network_id: 11155111, // Sepolia's id
+      gas: 5500000, // Sepolia has a lower block limit than mainnet
+      confirmations: 2, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
+    },
   },
   compilers: {
     solc: {
